ad72ea67a338fadf2ba8949a7edb09d222ca296c
Javier E. <jaedns@gmail.com>
2025-07-24 21:51:34 -0300

    Setup GH Actions workflow for CI (#1)
    * adding GH actions workflows
    
    * Setup GH Actions workflow for CI
    
    * Improve caching
    
    * Improve naming
    
    * Add a unit test
    
    * Use binstall

--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -6,6 +6,22 @@ use crate::components::PageTitle;
 pub fn HomePage() -> Element {
     rsx! {
         PageTitle { "Home" }
+
         h1 { class: "h1", "Home" }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn has_a_heading_text() {
+        let element = dioxus_ssr::render_element(HomePage());
+        let expected = dioxus_ssr::render_element(rsx! {
+            h1 { class: "h1", "Home" }
+        });
+
+        pretty_assertions::assert_str_eq!(element, expected);
     }
 }
