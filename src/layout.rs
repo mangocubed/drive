49147285fa26e004bbc3fefa385edcd269426c62
Javier E. <jaedns@gmail.com>
2025-07-25 18:19:18 -0300

    add loading overlay

--- src/layout.rs
+++ src/layout.rs
@@ -2,9 +2,22 @@ use dioxus::prelude::*;
 
 use crate::routes::Routes;
 
+const ICON_SVG: Asset = asset!("assets/icon.svg");
+
 #[component]
 pub fn Layout() -> Element {
+    let mut is_loading = use_signal(|| true);
+
+    use_effect(move || is_loading.set(false));
+
     rsx! {
         main { class: "main", Outlet::<Routes> {} }
+
+        div { class: "loading-overlay", class: if !is_loading() { "is-done" },
+            figure {
+                div { class: "loading-pulse" }
+                img { src: ICON_SVG }
+            }
+        }
     }
 }
