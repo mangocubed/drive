14ca781b9049d1d332a5ae93d70505a4151acf54
Javier E. <jaedns@gmail.com>
2025-07-25 16:27:11 -0300

    database initial setup (#2)
    * adding defsuk
    
    * database initial setup
    
    * restore cargo caching

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Lime3
 
 Cloud storage & digital distribution platform.
+
+### Environment variables
+
+| Name                     | Type    | Default                                         |
+| ------------------------ | ------- | ----------------------------------------------- |
+| DATABASE_MAX_CONNECTIONS | Integer | 5                                               |
+| DATABASE_URL             | String  | postgres://lime3:lime3@127.0.0.1:5432/lime3_dev |
---
+++ src/server/mod.rs
@@ -0,0 +1,21 @@
+use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
+use tokio::sync::OnceCell;
+
+mod config;
+
+use config::DATABASE_CONFIG;
+
+static DB_POOL_CELL: OnceCell<PgPool> = OnceCell::const_new();
+
+async fn db_pool<'a>() -> &'a PgPool {
+    DB_POOL_CELL
+        .get_or_init(|| async {
+            PgPoolOptions::new()
+                .max_connections(DATABASE_CONFIG.max_connections as u32)
+                .connect(&DATABASE_CONFIG.url)
+                .await
+                .expect("Failed to create DB pool.")
+        })
+        .await
+}
